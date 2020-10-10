82fe103e545d59f2a80997bcc18a174cafc0d1a6
dvnikolaev <dvnikolaeev@gmail.com>
2020-10-10 20:08:23 +0400

    add isAccept

--- src/models/IPost.ts
+++ src/models/IPost.ts
@@ -4,6 +4,7 @@ interface IPost {
   title: string;
   description: string;
   date: string;
+  isAccept: boolean;
 }
 
 export interface IPostState {
@@ -16,6 +17,7 @@ export class Post implements IPost {
   title: string;
   description: string;
   date: string = new Date().toISOString().substr(0,10);
+  isAccept: boolean = false;
 
   constructor(author_id: number, title: string, description: string) {
     this.author_id = author_id;
