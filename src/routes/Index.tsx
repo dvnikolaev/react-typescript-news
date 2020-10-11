ada7d9ae53292e5b65564ca2e751a636a510cbec
dvnikolaev <dvnikolaeev@gmail.com>
2020-10-11 22:59:42 +0400

    add description app

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import { Divider, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import { connect } from "react-redux";
 
-const IndexPage = () => {
-  return <div></div>
-}
+const useClasses = makeStyles((theme) => ({
+  pageWrapper: {
+    paddingTop: theme.spacing(2),
+  },
+}));
 
-export default IndexPage;
+const IndexPage: React.FC<{ username: string }> = ({ username }) => {
+  const classes = useClasses();
+
+  return (
+    <div className={classes.pageWrapper}>
+      <Typography variant="h5" paragraph>
+        Привет, <strong>{username}</strong>!
+      </Typography>
+      <Typography paragraph>
+        Для входа в систему используйте пары логин - пароль:
+      </Typography>
+      <Typography paragraph>user - user</Typography>
+      <Typography paragraph>admin - admin</Typography>
+      <Divider />
+      <Typography paragraph>
+        <strong>Гость</strong> может только просматривать одобренные новости.
+      </Typography>
+      <Typography paragraph>
+        <strong>Пользователь</strong> может добавлять новости. Видит только
+        одобренные и свои неодобренные новости.
+      </Typography>
+      <Typography paragraph>
+        <strong>Администратор</strong> может одобрить / отклонить новость. Видит
+        все новости.
+      </Typography>
+      <Typography>
+        По умолчанию созданная новость не является одобренной.
+      </Typography>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: any) => {
+  return {
+    username: state.auth.user.username ? state.auth.user.username : "Гость",
+  };
+};
+
+export default connect(mapStateToProps)(IndexPage);
