6ba9e2cb95a75be32d56ec13b02dc13c4ce668f9
dvnikolaev <dvnikolaeev@gmail.com>
2020-10-11 23:06:36 +0400

    Updates

---
+++ precache-manifest.0ad500a678840a40cebc630af0ffcb4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e0bcc39c43ebfe4deb88c385e35024",
+    "url": "/react-typescript-news/index.html"
+  },
+  {
+    "revision": "da705c4704de98ec583c",
+    "url": "/react-typescript-news/static/js/2.b965ae5d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-typescript-news/static/js/2.b965ae5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afa7584652711179bb1",
+    "url": "/react-typescript-news/static/js/main.7e6272b6.chunk.js"
+  },
+  {
+    "revision": "ae4427b2f40677db4890",
+    "url": "/react-typescript-news/static/js/runtime-main.429c8b47.js"
+  }
+]);
