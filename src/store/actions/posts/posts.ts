3b3b1f74df92e3ffafa17df41d3d86a0668d401b
dvnikolaev <dvnikolaeev@gmail.com>
2020-10-11 15:27:51 +0400

    add accept post

--- src/store/actions/posts/posts.ts
+++ src/store/actions/posts/posts.ts
@@ -1,5 +1,7 @@
+import { store } from '../../index';
+
 import { Post } from "../../../models/IPost";
-import { ADD_POST } from "./types";
+import { ACCEPT_POST, ADD_POST } from "./types";
 
 export const addPost = (author_id: number, title: string, description: string) => {
   const post = new Post(author_id,title,description);
@@ -7,5 +9,14 @@ export const addPost = (author_id: number, title: string, description: string) =
   return {
     type: ADD_POST,
     payload: post
+  }
+}
+
+export const onAcceptPost = (id: number) => {
+  const foundedIndex: number = store.getState().posts.posts.findIndex(item => item.id === id);
+
+  return {
+    type: ACCEPT_POST,
+    payload: foundedIndex
   }
 }
