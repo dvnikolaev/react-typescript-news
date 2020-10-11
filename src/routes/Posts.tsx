fabddd4e56d0b5904c77f5cf1caab17d4b3e5a1c
dvnikolaev <dvnikolaeev@gmail.com>
2020-10-11 20:21:25 +0400

    add classes

--- src/routes/Posts.tsx
+++ src/routes/Posts.tsx
@@ -1,10 +1,18 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 
 import PostsList from "../components/posts-list/posts-list";
 
+const useClasses = makeStyles((theme) => ({
+  pageWrapper: {
+    paddingTop: theme.spacing(2),
+  },
+}));
+
 const PagePosts = () => {
+  const classes = useClasses();
   return (
-    <div>
+    <div className={classes.pageWrapper}>
       <PostsList />
     </div>
   );
