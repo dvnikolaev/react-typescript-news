918c2776aa3f8363e1c8b2abf7f1830004047d2a
dvnikolaev <dvnikolaeev@gmail.com>
2020-10-11 15:26:40 +0400

    add posts reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import authReducer from "./authReducer";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  posts: postsReducer
 })
